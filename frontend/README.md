1891aba1999b3d013b0d4e8886714c305f960fba
mrdudov <mrdudov@gmail.com>
2023-05-04 15:45:44 +0300

    rename project to battleship

--- frontend/README.md
+++ frontend/README.md
@@ -1,4 +1,4 @@
-# 2048Game
+# battleship Game
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.0.0.
 
--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title '2048-game'`, () => {
+  it(`should have as title 'battleship-game'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('2048-game');
+    expect(app.title).toEqual('battleship-game');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('2048-game app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('battleship-game app is running!');
   });
 });
